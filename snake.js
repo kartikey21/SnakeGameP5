255c0007fec6fce211511cc0d2bc64540ae5c055
kartikey21 <kartikey.jain.21@outlook.com>
2021-03-02 12:18:01 +0530

    initial commit

---
+++ snake.js
@@ -0,0 +1,44 @@
+class Snake{
+  constructor(){
+    this.body  =  [];
+    this.body[0]  =  createVector(0,0);
+    this.xdir  =  0;
+    this.ydir  =  0;
+    this.len  =  1;
+  }
+  
+  setDir(x,y){
+    this.xdir  =  x;
+    this.ydir  =  y;
+  }
+  
+  grow() {
+  	let head = this.body[this.body.length-1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+  
+  eat(pos) {
+  	let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if(x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+  update() {
+  	let head = this.body[this.body.length-1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+  show(){
+    for(var i=0;i<this.body.length;i++){
+    fill(0);
+    noStroke();
+    rect(this.body[i].x,this.body[i].y,1,1);
+    }
+  }
+}
